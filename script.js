b396de2f600c5549946a36eb45f90bd3e15b668f
yanntyb <69966391+yanntyb@users.noreply.github.com>
2021-01-11 16:45:40 +0100

    a

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 $("button").click(function (){
-    $("div").not(".global").not("#matrice").toggleClass("plein");
+    $("div").not(".global").not("#matrice").not(".ligne").toggleClass("plein");
 })
 
 $(".circle").click(function (){
     $(this).toggleClass("plein");
+})
+
+$("div").not(".global").not("#matrice").not(".ligne").not(".circle").click(function (){
+    $(this).parent().children().addClass(this.className);
 })
